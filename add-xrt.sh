031eb3a2cb65431bd3a06051a6e444be867bd925
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-29 15:41:05 +0800

    Update add-xrt.sh

--- add-xrt.sh
+++ add-xrt.sh
@@ -413,5 +413,5 @@ echo -e "Created On           : $hariini"
 echo -e "Expired On           : $exp"
 echo -e "═══════════════════"
 echo -e ""
-echo -e "Autoscript By NevermoreSSH"
-echo -e "" 
+echo -e ""
+echo -e "Script Mod By NevermoreSSH"

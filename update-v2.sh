c1637a4dfb9235e55bef5d343c3ba6a6e23f23a9
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-30 20:01:47 +0800

    Update update-v2.sh

--- update-v2.sh
+++ update-v2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Script Updater By NevermoreSSH
+#OnigiriWebsocketPremium-V2
 red='\e[1;31m'
 green='\e[0;32m'
 purple='\e[0;35m'
@@ -26,20 +27,20 @@ if [[ -e /usr/local/bin/xray ]]; then
 	 rm limit
 	 rm bbr
 	 rm menu
-	 #rm add-tr
-         #rm add-vless
-         #rm add-ws
-         #rm user-tr
-         #rm user-vless
+	 rm add-tr
+         rm add-vless
+         rm add-ws
+         rm user-tr
+         rm user-vless
 	 wget -O nf "https://${Server_URL}/media.sh" && chmod +x nf
 	 wget -O ins-helium "https://${Server_URL}/ins-helium.sh" && chmod +x ins-helium
 	 wget -O menu "https://${Server_URL}/menu.sh" && chmod +x menu
 	 wget -O limit "https://${Server_URL}/limit-speed.sh" && chmod +x limit
-	 #wget -O add-tr "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/add-tr.sh" && chmod +x add-tr
-	 #wget -O add-vless "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/add-vless.sh" && chmod +x add-vless
-	 #wget -O add-ws "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/add-ws.sh" && chmod +x add-ws
-	 #wget -O user-tr "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/user-tr.sh" && chmod +x user-tr
-	 #wget -O user-vless "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/user-vless.sh" && chmod +x user-vless
+	 wget -O add-tr "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/add-tr.sh" && chmod +x add-tr
+	 wget -O add-vless "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/add-vless.sh" && chmod +x add-vless
+	 wget -O add-ws "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/add-ws.sh" && chmod +x add-ws
+	 wget -O user-tr "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/user-tr.sh" && chmod +x user-tr
+	 wget -O user-vless "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/user-vless.sh" && chmod +x user-vless
 	 wget -O bbr "https://${Server_URL}/bbr.sh" && chmod +x bbr
 	 echo ""
 	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
@@ -67,20 +68,20 @@ else
 	 rm limit
 	 rm bbr
 	 rm menu
-	 #rm add-tr
-         #rm add-vless
-         #rm add-ws
-         #rm user-tr
-         #rm user-vless
+	 rm add-tr
+         rm add-vless
+         rm add-ws
+         rm user-tr
+         rm user-vless
 	 wget -O nf "https://${Server_URL}/media.sh" && chmod +x nf
 	 wget -O ins-helium "https://${Server_URL}/ins-helium.sh" && chmod +x ins-helium
 	 wget -O menu "https://${Server_URL}/menu.sh" && chmod +x menu
 	 wget -O limit "https://${Server_URL}/limit-speed.sh" && chmod +x limit
-	 #wget -O add-tr "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/add-tr.sh" && chmod +x add-tr
-	 #wget -O add-vless "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/add-vless.sh" && chmod +x add-vless
-	 #wget -O add-ws "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/add-ws.sh" && chmod +x add-ws
-	 #wget -O user-tr "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/user-tr.sh" && chmod +x user-tr
-	 #wget -O user-vless "https://raw.githubusercontent.com/NevermoreSSH/OnigiriWebsocketPremium-V2/main/XRAY/user-vless.sh" && chmod +x user-vless
+	 wget -O add-tr "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/add-tr.sh" && chmod +x add-tr
+	 wget -O add-vless "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/add-vless.sh" && chmod +x add-vless
+	 wget -O add-ws "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/add-ws.sh" && chmod +x add-ws
+	 wget -O user-tr "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/user-tr.sh" && chmod +x user-tr
+	 wget -O user-vless "https://raw.githubusercontent.com/NevermoreSSH/yourpath/main/XRAY/user-vless.sh" && chmod +x user-vless
 	 wget -O bbr "https://${Server_URL}/bbr.sh" && chmod +x bbr
 	 echo ""
 	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"

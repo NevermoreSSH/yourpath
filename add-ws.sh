75878a5c57b828f5799cb43d7488742cea84aae4
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-30 13:36:23 +0800

    Update add-ws.sh

--- add-ws.sh
+++ add-ws.sh
@@ -34,7 +34,7 @@ domain=$(cat /root/domain)
 MYIP2=$(wget -qO- ipv4.icanhazip.com);
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \E[0;47;30m     Add XRAY Vmess WS Account     \E[0m"
+echo -e "\\E[0;47;30m     Add XRAY Vmess WS Account     \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
                 read -rp "Username : " -e user
@@ -43,7 +43,7 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
                 if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
 		                echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                        echo -e " \E[0;47;30m     Add XRAY Vmess WS Account     \E[0m"
+                        echo -e "\\E[0;47;30m     Add XRAY Vmess WS Account     \E[0m"
                         echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                         echo ""
                         echo "A client with the specified name was already created, please choose another name."

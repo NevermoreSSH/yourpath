4b8a90d301400d5c96509203b422e6669cc9e647
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-30 11:44:47 +0800

    Update menu-xrt.sh

--- menu-xrt.sh
+++ menu-xrt.sh
@@ -10,15 +10,15 @@ B='\033[0;36m'
 N='\e[0m'
 clear
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;41;36m          XRAY Trojan TCP XTLS MENU         \E[0m"
+echo -e " \E[0;41;36m          XRAY Vless TCP XTLS MENU         \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m
 
- [\033[1;36m•1 \033[0m]  Add XRAY Trojan TCP XTLS Account
- [\033[1;36m•2 \033[0m]  Add Trial XRAY Trojan TCP XTLS Account
- [\033[1;36m•3 \033[0m]  Check User Login XRAY Trojan TCP XTLS
- [\033[1;36m•4 \033[0m]  Delete XRAY Trojan TCP XTLS Account
- [\033[1;36m•5 \033[0m]  Renew XRAY Trojan TCP XTLS Account
- [\033[1;36m•6 \033[0m]  Check XRAY Trojan TCP XTLS Config
+ [\033[1;36m•1 \033[0m]  Add XRAY Vless TCP XTLS Account
+ [\033[1;36m•2 \033[0m]  Add Trial XRAY Vless TCP XTLS Account
+ [\033[1;36m•3 \033[0m]  Check User Login XRAY Vless TCP XTLS
+ [\033[1;36m•4 \033[0m]  Delete XRAY Vless TCP XTLS Account
+ [\033[1;36m•5 \033[0m]  Renew XRAY Vless TCP XTLS Account
+ [\033[1;36m•6 \033[0m]  Check XRAY Vless TCP XTLS Config
 
  [\033[1;36m•0 \033[0m]  Back To Main Menu"
 echo ""

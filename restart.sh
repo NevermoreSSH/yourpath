8202109233395f6c3c1ddc4f1d339e2bd95e68f1
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-30 14:26:47 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -5,6 +5,11 @@
 # Auther  : NevermoreSSH
 # (C) Copyright 2022
 # =========================================
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 clear
 fun_bar() {
     CMD[0]="$1"

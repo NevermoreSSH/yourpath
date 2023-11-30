3943996b0871a1a1bb21ebc5cf07c2193f3d246d
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-30 10:16:53 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -95,3 +95,6 @@ echo -e "  \033[1;91m Service Restart Trojan TCP TLS\033[1;37m"
 fun_bar 'res11'
 echo -e ""
 echo -e "Script Mod By NevermoreSSH"
+echo ""
+read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} Back to menu . . .") "
+menu

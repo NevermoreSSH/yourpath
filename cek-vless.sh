1fa8663f22028399cedaaf14707cf97d4863c6cd
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-30 13:12:37 +0800

    Update cek-vless.sh

--- cek-vless.sh
+++ cek-vless.sh
@@ -12,7 +12,7 @@ NC='\e[0m'
 echo -n > /tmp/other.txt
 data=( `cat /usr/local/etc/xray/vless.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\\E[0;41;36m     XRAY Vless WS User Login      \E[0m"
+echo -e " \E[0;47;30m     XRAY Vless WS User Login      \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 for akun in "${data[@]}"
 do

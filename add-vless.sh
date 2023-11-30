b99c8d29cbec9492893956b1e0544fee822dd63f
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-30 13:36:06 +0800

    Update add-vless.sh

--- add-vless.sh
+++ add-vless.sh
@@ -22,7 +22,7 @@ domain=$(cat /root/domain)
 MYIP2=$(wget -qO- ipv4.icanhazip.com);
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo -e " \E[0;47;30m     Add XRAY Vless WS Account     \E[0m"
+        echo -e "\\E[0;47;30m     Add XRAY Vless WS Account     \E[0m"
         echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
 		read -rp "Username : " -e user
@@ -31,7 +31,7 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 clear
 		    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-            echo -e " \E[0;47;30m      Add XRAY Vless Account       \E[0m"
+            echo -e "\\E[0;47;30m      Add XRAY Vless Account       \E[0m"
             echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 			echo ""
 			echo "A client with the specified name was already created, please choose another name."
